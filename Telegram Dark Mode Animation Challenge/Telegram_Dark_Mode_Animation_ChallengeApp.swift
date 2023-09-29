561b086fba0524c8ab6dec662f4c047acf14bf93
Kartikeya Saxena Jain <kartikeya.saxena.jain@gmail.com>
2023-09-29 11:58:46 -0400

    Initial Commit

---
+++ Telegram Dark Mode Animation Challenge/Telegram_Dark_Mode_Animation_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Telegram_Dark_Mode_Animation_ChallengeApp.swift
+//  Telegram Dark Mode Animation Challenge
+//
+//  Created by Kartikeya Saxena Jain on 9/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Telegram_Dark_Mode_Animation_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
