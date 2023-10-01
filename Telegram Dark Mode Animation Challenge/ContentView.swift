6453f7b03e58860e53ab23997026893ff4abc01b
Kartikeya Saxena Jain <kartikeya.saxena.jain@gmail.com>
2023-10-01 15:24:37 -0400

    Setup view basics

--- Telegram Dark Mode Animation Challenge/ContentView.swift
+++ Telegram Dark Mode Animation Challenge/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var activeTab: Int = 0
+    @State private var toggles: [Bool] = Array(repeating: false, count: 10)
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        TabView(selection: $activeTab,
+                content:  {
+            // TAB 1
+            NavigationStack {
+                List {
+                    Section("Text Section") {
+                        Toggle("Large Display", isOn: $toggles[0])
+                        Toggle("Bold Text", isOn: $toggles[1])
+                    }
+                    
+                    Section {
+                        Toggle("Night Light", isOn: $toggles[2])
+                        Toggle("True Tone", isOn: $toggles[3])
+                    } header: {
+                        Text("Display Section")
+                    } footer: {
+                        Text("Footer Text")
+                    }
+                }
+                .navigationTitle("Dark Mode")
+            }
+            .tabItem {
+                Image(systemName: "house")
+                Text("Home")
+            }
+            
+            // TAB 2
+            Text("Settings")
+                .tabItem {
+                    Image(systemName: "gearshape")
+                    Text("Settings")
+                }
+        })
     }
 }
 
